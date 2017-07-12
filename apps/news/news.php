8e18fba9670f14d8b6fddb375e4d57d7b5dbdabf
Juha Untinen <juha.untinen@leaddesk.com>
2017-07-12 12:58:55 +0000

    HTTP method handling added and some mockup responses

--- apps/news/news.php
+++ apps/news/news.php
@@ -1,9 +1,45 @@
 <?php
 
-echo "News!<hr/>";
-print_r($_GET);
-$things = explode('/', $_GET['params']);
+/*
+    This is generally used to handle News items. News will also have comments, which are stored
+    in a different table than the news themselves. They are matched by news ID as foreign key.
+*/
 
-foreach ($things as $thing) {
-    echo $thing."<br/>";
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Get the possible parameters, eg. /news/123 has the parameter "123" for News ID
+$parameters = explode('/', $_GET['params']);
+
+// News ID, eg. GET /news/123
+$id = $parameters[1];
+
+// The action is generally reserved for getting the comments for a given news ID eg.
+// GET /news/123/comments
+// where "comments" is the action.
+$action = $parameters[2];
+
+switch ($method) {
+    case 'GET':
+        // Either all news or a specific news ID
+        $resp = array("get hello" => "world");
+        break;
+    case 'POST':
+        // Add news
+        $resp = array("post" => "manpat");
+        break;
+    case 'PUT':
+        // Update an existing news ID, eg. PUT /news/123
+        // with a payload JSON that has the update information
+        $resp = array("updating" => "stuff");
+        break;
+    case 'DELETE':
+        // Delete a news ID, eg. DELETE /news/123
+        return http_response_code(204);
+        break;
+    default:
+        $resp = array("dafuq" => "No idea what you tried");
+        break;
 }
+
+header('Content-Type: application/json');
+echo json_encode($resp);
